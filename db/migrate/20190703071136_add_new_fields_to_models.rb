ded43d365d969a4704657b6fcc653f1033ab161e
Bhuvana Prabhu <bhuvna@joshsoftware.com>
2019-07-03 12:48:10 +0530

    Added pin to staff model

--- db/migrate/20190703071136_add_new_fields_to_models.rb
+++ db/migrate/20190703071136_add_new_fields_to_models.rb
@@ -1,9 +1,8 @@
-# frozen_string_literal: true
-
-class AddFieldsToStudent < ActiveRecord::Migration[5.2]
+class AddNewFieldsToModels < ActiveRecord::Migration[5.2]
   def change
     add_column :students, :driver_name, :string
     add_column :students, :driver_number, :string
     add_column :students, :preferred_language, :string
+    add_column :staffs, :pin, :string
   end
 end
