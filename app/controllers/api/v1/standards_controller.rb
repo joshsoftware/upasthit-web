9076c8a7530d61098b22e5d9e2adcf9ca428657b
Bhuvana Prabhu <bhuvna@joshsoftware.com>
2019-07-09 15:20:10 +0530

    Rubocop offences removed

--- app/controllers/api/v1/standards_controller.rb
+++ app/controllers/api/v1/standards_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module Api
-  class V1::StandardsController < V1::BaseController
-    def index
-      res = Standard.pluck(:id, :standard, :section)
-      records = res.each_with_object({}) {|std, result| r = {std[1] => [{name: std[2], id: std[0]}]}; result.merge!(r) {|_std, old, new| old + new }; }
-      render json: {data: records, status: 200}
+  module V1
+    class StandardsController < V1::BaseController
+      def index
+        res = Standard.pluck(:id, :standard, :section)
+        records = res.each_with_object({}) {|std, result| r = {std[1] => [{name: std[2], id: std[0]}]}; result.merge!(r) {|_std, old, new| old + new }; }
+        render json: {data: records, status: 200}
+      end
     end
   end
 end
--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 module Api
-  class V1::StudentsController < V1::BaseController
-    def index
-      school_id = Standard.find(params[:standard_id]).school_id
-      students = Student.where(standard_id: params[:standard_id], school_id: school_id)
-      standard_attendance = StandardAttendance.where(standard_id: params[:standard_id], school_id: school_id, date: Date.today.strftime)
-      status = standard_attendance.count != 0 ? standard_attendance.first.attendance_marked : false
-      render_success(data: {students:         students.as_json(only:    %i[roll_no name gender standard_id],
-                                                               methods: [:attendance_status]),
-                            attendance_taken: status}, status: 200)
+  module V1
+    class StudentsController < V1::BaseController
+      def index
+        school_id = Standard.find(params[:standard_id]).school_id
+        students = Student.where(standard_id: params[:standard_id], school_id: school_id)
+        standard_attendance = StandardAttendance.where(standard_id: params[:standard_id], school_id: school_id, date: Date.today.strftime)
+        status = standard_attendance.count != 0 ? standard_attendance.first.attendance_marked : false
+        render_success(data: {students:         students.as_json(only:    %i[roll_no name gender standard_id],
+                                                                 methods: [:attendance_status]),
+                              attendance_taken: status}, status: 200)
+      end
     end
   end
 end
