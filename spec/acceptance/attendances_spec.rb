69f3a3dd678ab3725f831e9deb83ba05f1077ae9
Bhuvana Prabhu <bhuvna@joshsoftware.com>
2019-07-17 13:36:53 +0530

    Added example of date

--- spec/acceptance/attendances_spec.rb
+++ spec/acceptance/attendances_spec.rb
@@ -8,7 +8,7 @@ resource "Attendances" do
   let(:school) { create(:school) }
   let!(:staff_1) { create(:staff_with_standards, school_id: school.id) }
   let!(:staff_2) { create(:staff_with_standards, school_id: school.id) }
-
+  let!(:from_date) { (DateTime.now - 1.month).strftime("%d/%m/%Y") }
   before do
     SchoolTiming.days.keys.each do |day|
       FactoryBot.create(:school_timing, school_id: school.id, day: day)
@@ -16,9 +16,9 @@ resource "Attendances" do
   end
 
   get "v1/attendances/sync", document: :v1 do
-    parameter :date, "Start date (end date is current date)", required: true
+    parameter :date, "Start date (end date is current date)", required: true, example: "01/01/2019"
     parameter :school_id, "School ID", required: true
-    let!(:date) { DateTime.now - 7.months }
+    let!(:date) { from_date }
     let!(:school_id) { school.id }
     let(:raw_post) { params.to_json }
     example "Sync API" do
--- spec/controllers/api/v1/attendances_controller_spec.rb
+++ spec/controllers/api/v1/attendances_controller_spec.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+
 RSpec.describe Api::V1::AttendancesController, type: :controller do
   let!(:school) { create(:school) }
   let!(:staff) { create(:staff_with_standards, school_id: school.id) }
+  let!(:date) { (DateTime.now - 1.month).strftime("%d/%m/%Y") }
+  let(:future_date) { (DateTime.now + 3.months).strftime("%d/%m/%Y") }
 
   describe "GET : sync API" do
     context "On success" do
       it "should return json of attendances starting from date given till currrent date" do
-        get :sync, params: {school_id: school.id, date: DateTime.now - 1.month}
+        get :sync, params: {school_id: school.id, date: date}
         expect(response.status).to eq(200)
         response_body = JSON.parse(response.body)
         expect(response_body["attendances"].count).to eq 2
@@ -22,11 +25,33 @@ RSpec.describe Api::V1::AttendancesController, type: :controller do
         response_body = JSON.parse(response.body)
         expect(response_body["message"]).to eq "Please enter date"
       end
+
+      it "date format is invalid" do
+        get :sync, params: {school_id: school.id, date: "12.01.2019"}
+        expect(response.status).to eq(422)
+        response_body = JSON.parse(response.body)
+        expect(response_body["message"]).to eq "Invalid Date"
+      end
+
+      it "date given is in future" do
+        get :sync, params: {date: future_date, school_id: school.id}
+        expect(response.status).to eq(400)
+        response_body = JSON.parse(response.body)
+        expect(response_body["errors"]["base"]).to eq ["Date cannot be in future"]
+      end
+
       it "school_id is not given in params" do
-        get :sync, params: {date: DateTime.now}
+        get :sync, params: {date: date}
         expect(response.status).to eq(400)
         response_body = JSON.parse(response.body)
         expect(response_body["errors"]["school_id"]).to eq ["can't be blank"]
+      end
+
+      it "school_id is not valid" do
+        get :sync, params: {date: date, school_id: 11_212}
+        expect(response.status).to eq(400)
+        response_body = JSON.parse(response.body)
+        expect(response_body["errors"]["base"]).to eq ["Invalid School ID"]
       end
     end
   end
